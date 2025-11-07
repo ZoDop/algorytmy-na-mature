6911ecdc24cc6ecc1a7e028ffe25ababce316562
ZoDop <133035137+ZoDop@users.noreply.github.com>
2025-11-07 21:31:12 +0100

    Update liczby pierwsze.py

--- liczby pierwsze.py
+++ liczby pierwsze.py
@@ -3,9 +3,10 @@ def prime(n):
     if n < 2:
         return False
     sqrt = n ** 0.5
-    for i in range(2, sqrt + 1):
+    for i in range(2, int(sqrt) + 1):
         if n % i == 0:
             return False
     return True
+
 
 prime(5)
