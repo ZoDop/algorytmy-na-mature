9918c00377e54ba9c98eaec90ab01062c703a3d1
ZoDop <133035137+ZoDop@users.noreply.github.com>
2025-11-29 12:39:29 +0100

    Update szyfr_cezara.py
    dodaje algorytm z maturait

--- szyfr_cezara.py
+++ szyfr_cezara.py
@@ -8,4 +8,28 @@ def cezar(text, key):
             coded += chr((ord(i) - 97 + key) % 26 + 97)
     return coded
 
+
 print(cezar('abcd zefa', 1))
+
+
+
+def szyfruj(slowo, klucz):
+    zaszyfrowane_slowo = ""
+    for i in range(len(slowo)):
+            klucz = klucz % 26
+            znak = chr(ord(slowo[i]) + klucz)
+            if znak > 'z':         # przypadek w którym znak wyszedł poza alfabet
+                znak = chr(ord(znak) - 26)
+            zaszyfrowane_slowo += znak
+    return zaszyfrowane_slowo
+
+
+    def odszyfruj(slowo, klucz):
+    odszyfrowane_slowo = ""
+    for i in range(len(slowo)):
+            klucz = klucz % 26
+            znak = chr(ord(slowo[i]) - klucz)
+            if znak < 'a':         # przypadek w którym znak wyszedł poza alfabet
+                znak = chr(ord(znak) + 26)
+            odszyfrowane_slowo += znak
+    return odszyfrowane_slowo
